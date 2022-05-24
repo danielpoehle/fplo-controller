ecf7f417c7f41340f6a367cd567302be7f94f9bb
danielpoehle <d.poehle@gmx.de>
2022-05-24 21:32:13 +0200

    add summary table for Fplos

--- app.js
+++ app.js
@@ -22,17 +22,52 @@
         fploList.showWork = function(){
             fploList.Work = [];
             if(fploList.KanBan.length > 0 && fploList.ABFKInstance.length > 0){                
-                for (let kw = fploList.currentWeek-4; kw < fploList.currentWeek+8; kw+=1) { 
+                for (let kw = fploList.currentWeek-4; kw < fploList.currentWeek+4; kw+=1) { 
                     if(kw < 1 || kw > 52){continue;}
+
                     let abfk = fploList.ABFKInstance.filter((a) => a.BOB_DATE.Year === fploList.selectedYear && 
                                                                    a.BOB_DATE.KW === kw &&
-                                                                   a.Vorgang >= 50000 && a.Vorgang < 60000);
-                    console.log(abfk);
+                                                                   a.Vorgang >= 50000 && a.Vorgang < 60000 &&
+                                                                   (a['Trassen FPLO'] + a['Trassen ÜB'] > 0));
+                    //console.log(abfk);
+                    let vorgang = abfk.map((a) => a.Vorgang);
+                    vorgang = vorgang.filter((item, index) => vorgang.indexOf(item)===index);
+                    let vgList = [];
+                    for (let j = 0; j < vorgang.length; j+=1) {                        
+                        let vg = abfk.filter((a) => a.Vorgang === vorgang[j]);
+                        let tr_fplo = vg.map((a) => a['Trassen FPLO']).reduce((partialSum, a) => partialSum + a, 0);
+                        let done_fplo = vg.map((a) => a['Fertig FPLO']).reduce((partialSum, a) => partialSum + a, 0);
+                        let tr_ub = vg.map((a) => a['Trassen ÜB']).reduce((partialSum, a) => partialSum + a, 0);
+                        let done_ub = vg.map((a) => a['Fertig ÜB']).reduce((partialSum, a) => partialSum + a, 0);
+                        for (let k = 0; k < vg.length; k+=1) {
+                            let staff = 'Kanban fehlt!';
+                            let sel_kanban = fploList.KanBan.findIndex((a) => a.Vorgang === vg[k].Vorgang &&
+                                                                     a.EVU === vg[k]['EVU Gruppe'] &&
+                                                                     a['Fahrplan-\njahr'] === fploList.selectedYear);
+                            
+                            if(sel_kanban >= 0){staff = fploList.KanBan[sel_kanban].Bearbeiter;}
+                            vg[k].Staff = staff;
+                        }
+                        vgList.push({
+                            'ANZ_DOK': vg.length,
+                            'VORGANG': vorgang[j],
+                            'TR_FPLO': tr_fplo,
+                            'OPEN_FPLO': tr_fplo - done_fplo,
+                            'TR_UB': tr_ub,
+                            'OPEN_UB': tr_ub - done_ub,
+                            'DOK': vg
+                        });
+                    }
+                    let sum_dok = vgList.map((a) => a.ANZ_DOK).reduce((partialSum, a) => partialSum + a, 0);
                     fploList.Work.push({
-                        'KW': kw
+                        'KW': kw,
+                        'ANZ_MN': vorgang.length,
+                        'SUM_DOK': sum_dok,
+                        'MN': vgList
                     });
                 }
                 fploList.loadComplete = true;
+                //console.log(fploList.Work);
             }else{
                 console.log("At least one selected list is empty.");
             }
@@ -58,7 +93,8 @@
                 df[i]['BOB_DATE'] = {'DText': tm.toLocaleString(), 'DNumber': tm.ts, 'Original': origTxt, 'KW': tm.weekNumber, 'Year': tm.year};
             }
           
-            /* DO SOMETHING WITH workbook HERE */            
+            /* DO SOMETHING WITH workbook HERE */ 
+            //console.log(df[0]);           
             fploList.ABFKInstance = df;
           };
 
@@ -76,8 +112,9 @@
             
           
             /* DO SOMETHING WITH workbook HERE */            
-            //console.log(df);
+            console.log(df);
             fploList.KanBan = df;
+            console.log(Object.keys(fploList.KanBan[0]));
           };
              
     };
